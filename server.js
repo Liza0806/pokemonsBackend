ec07a22fde34047984e203ff4abde32940a1af35
Liza <fildepiorsta@gmail.com>
2024-04-06 14:39:16 +0300

    move db_host

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose")
 const app = require('./app')
-const DB_HOST = "mongodb+srv://PokemonLiza:DkAJQHc9tf3PXegf@cluster0.we1apxr.mongodb.net/pokemons?retryWrites=true&w=majority&appName=Cluster0"
-
+const {DB_HOST} = process.env
 mongoose.connect(DB_HOST).then(()=>{
     app.listen(3001, ()=> {
         console.log('Server running. Use our API on port: 3002')
